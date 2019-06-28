9bf7d475234aebe6fe1376efee455617a679c331
distortedecho <43297676+distortedecho@users.noreply.github.com>
2019-06-28 17:54:19 +0530

    made responsive

---
+++ javascript.js
@@ -0,0 +1,31 @@
+function myFunction(x){
+	x.classList.toggle("change")
+	var y= document.getElementsByClassName("sidenav")[0].style.width;
+  if(y=="70%"){
+  	document.getElementById("Sidenav").style.width = "0%";
+	document.getElementById("divone").style.backgroundColor = "#FFF0F3";
+	document.getElementById('menuc').innerHTML="MENU";
+	document.getElementById("divimg").style.backgroundImage="url(img/bg-style-1.jpg)";
+    }else{
+    document.getElementById("Sidenav").style.width = "70%";
+	document.getElementById("divone").style.backgroundColor = "white";
+	document.getElementById("divimg").style.backgroundImage="none";
+	document.getElementById('menuc').innerHTML="CLOSE";
+}
+}
+function newFunction(x){
+	x.classList.toggle('change2')
+}
+//adding jquery
+$('.addon3').on('click',function(){
+	$('html,body').animate({                         /*offset gives the coordinates of the selected elements*/ 
+     'scrollTop':$('.newdiv').offset().top        /*ie top and left*/
+	},1000);
+});
+$(document).ready(function(){
+$('.projectcontainer').on('click',function(){
+	$('html,body').animate({                         /*offset gives the coordinates of the selected elements*/ 
+     'scrollTop':$('.contentmain').offset().top        /*ie top and left*/
+	},1000);
+});
+});
